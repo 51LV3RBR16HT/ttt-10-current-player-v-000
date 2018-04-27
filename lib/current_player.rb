ee35abeb680adb1603296c5ec94a9d1500ae68f0
Barbara Washington <barbara.m.washington@gmail.com>
2018-04-27 23:17:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,14 @@
 def turn_count(board)
-  counter = 0
-  board.each do |token|
+  #counter needs to start at 0 to keep track of turns
+  counter = 0 
+  
+  board.each do |token| 
     if token == "X" || token == "O"
+  #counter will need to increase by 1 to track each turn  
     counter += 1
     end
   end
+  #call the counter to get the number of turns
   counter
 end
 
@@ -15,4 +19,9 @@ def current_player(board)
   #   return "X"
   # else
   #   return "O"
+  #you can also use a modulo (%) to get odd or even
+  #but it's just easier to use an iterator (.each) or 
+  #or better to use a ternary expression.
+  
+  #
 end
