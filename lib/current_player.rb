c22a795dedaf926f10394b2d547a934551bba9e2
Barbara Washington <barbara.m.washington@gmail.com>
2018-04-27 23:20:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   #counter needs to start at 0 to keep track of turns
   counter = 0 
-  #We declare a local variable 'token' by enclosing it in | | pipes. This variable's value is automatically assigned the element from the array for the current iteration.
+  #We declare a local variable 'token' by enclosing it in | |
+  #pipes. This variable's value is automatically assigned the
+  #element from the array for the current iteration.
   board.each do |token| 
     if token == "X" || token == "O"
   #counter will need to increase by 1 to track each turn  
@@ -14,14 +16,12 @@ end
 
 def current_player(board) 
   turn_count(board).even? ? "X" : "O"
-  ### Or can be written as: 
+  # Or can be written as: 
   # if turn_count(board).even? 
   #   return "X"
   # else
   #   return "O"
-  #you can also use a modulo (%) to get odd or even
-  #but it's just easier to use an iterator (.each) or 
-  #or better to use a ternary expression.
-  
-  #
+  #you can also use a modulo (%) to get an odd or even number
+  #but it's easier to use an iterator (.each)
+  #and even better to use the ternary expression above.
 end
